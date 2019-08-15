095b52e6d4ef2ade7292b51515bb48f7e65ed202
Z <sword-dancer@yandex.ru>
2019-08-15 17:50:06 +0300

    added selenium to retrieve source pic, locators in external module

---
+++ locators.py
@@ -0,0 +1,7 @@
+"""
+This module contains selectors for Selenium
+"""
+from selenium.webdriver.common.by import By
+
+FLICK_PIC = (By.CSS_SELECTOR, ".view.photo-notes-scrappy-view")
+FLICK_PIC_ZOOM = (By.CSS_SELECTOR, ".zoom-large")
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
+"""Main module to do all the stuff."""
 from time import time
 from urllib.request import urlopen, urlretrieve
 from getpass import getuser
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import locators
+
+
+BROWSER = webdriver.Chrome()
+BROWSER.implicitly_wait(10)
+ACTION = ActionChains(BROWSER)
+
+
+def interface(url):
+    """Get common url of pic and retrieve the original source image"""
+    BROWSER.get(url)
+    elem = BROWSER.find_element(*locators.FLICK_PIC)
+    elem.click()
+    ACTION.move_to_element(elem).perform()
+    pic = BROWSER.find_element(*locators.FLICK_PIC_ZOOM).get_attribute('src')
+    get_img(pic)
+    BROWSER.close()
 
 
 def get_img(url, loc='C:/Users/'+getuser()+'/Desktop/'):
+    """Download the image to selected location"""
     response = urlopen(url)
-    assert response.code == 200
+    assert response.code == 200, "Can't open image. Maybe link is broken"
     urlretrieve(url, loc+str(int(time()))+'.png')
 
 
-link = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-get_img(link)
+def main():
+    """Main function gets user url and starts the program"""
+    link = 'https://www.flickr.com/photos/kristianbell/48016130083/in/album-72157625507310459/'
+    interface(link)
+
+
+main()
