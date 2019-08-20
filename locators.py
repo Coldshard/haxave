7b48bc467083e957a9f82d5cbdc1d7a0fe3eb300
Z <sword-dancer@yandex.ru>
2019-08-20 18:32:27 +0300

    Album pics parsing by url, getting all content of dynamic page

--- locators.py
+++ locators.py
@@ -5,3 +5,5 @@ from selenium.webdriver.common.by import By
 
 FLICK_PIC = (By.CSS_SELECTOR, ".view.photo-notes-scrappy-view")
 FLICK_PIC_ZOOM = (By.CSS_SELECTOR, ".zoom-large")
+FLICK_ALBUM_PIC_SELECT = (By.CSS_SELECTOR, ".overlay")
+FLICK_ALBUM_PICS_COUNT = (By.CSS_SELECTOR, ".photo-counts")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Main module to do all the stuff."""
-from time import time
+from time import time, sleep
+import re
 from urllib.request import urlopen, urlretrieve
 from getpass import getuser
 from selenium import webdriver
@@ -12,28 +13,65 @@ BROWSER.implicitly_wait(10)
 ACTION = ActionChains(BROWSER)
 
 
-def interface(url):
+def interface(url, url_type='pic'):
+    """Starts execution"""
+    BROWSER.get(url)
+    pics = parse_album(url) if url_type == 'album' else url
+    for img in pics:
+        get_img(img)
+    BROWSER.close()
+
+
+def get_source(url):
     """Get common url of pic and retrieve the original source image"""
     BROWSER.get(url)
     elem = BROWSER.find_element(*locators.FLICK_PIC)
     elem.click()
     ACTION.move_to_element(elem).perform()
     pic = BROWSER.find_element(*locators.FLICK_PIC_ZOOM).get_attribute('src')
-    get_img(pic)
-    BROWSER.close()
+    return pic
+
+
+def scroll_to_end():
+    """Scrolls dynamic content to the end of page"""
+    pause = 1
+    last_height = BROWSER.execute_script("return document.body.scrollHeight")
+    while True:
+        BROWSER.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(pause)
+        new_height = BROWSER.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def parse_album(album_url):
+    """Find all images in album and return their urls"""
+    BROWSER.get(album_url)
+    scroll_to_end()
+    lst = []
+    pics = BROWSER.find_elements(*locators.FLICK_ALBUM_PIC_SELECT)
+    for elem in pics:
+        lst.append(elem.get_attribute('href'))
+    cnt = BROWSER.find_element(*locators.FLICK_ALBUM_PICS_COUNT).text
+    assert len(lst) == int(re.search(r'\d+', cnt).group())
+    return lst
 
 
 def get_img(url, loc='C:/Users/'+getuser()+'/Desktop/'):
     """Download the image to selected location"""
-    response = urlopen(url)
+    img = get_source(url)
+    response = urlopen(img)
     assert response.code == 200, "Can't open image. Maybe link is broken"
-    urlretrieve(url, loc+str(int(time()))+'.png')
+    pic_name = str(int(time()))+'.png'
+    urlretrieve(img, loc+pic_name)
+    print(f'Saved {img} to {loc} as {pic_name}')
 
 
 def main():
     """Main function gets user url and starts the program"""
-    link = 'https://www.flickr.com/photos/kristianbell/48016130083/in/album-72157625507310459/'
-    interface(link)
+    link = 'https://www.flickr.com/photos/kristianbell/albums/72157625584774754'
+    interface(link, 'album')
 
 
 main()
